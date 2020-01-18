5a1a1c2353ae6431730cc1e5d6dc780189306c37
A_ito <BoxPistols@users.noreply.github.com>
2020-01-19 07:55:43 +0900

    add Item
     v-on:submit.prevent

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,12 +1,35 @@
-<template lang="pug">
-  div.todoBox
-    p test..
-    
+<template>
+  <div class="box">
+    <form v-on:submit.prevent>
+      <input type="text">
+      <button v-on:click="addItem">Add</button>
+    </form>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello"
+    };
+  },
+  methods: {
+    addItem: function(event) {
+      alert();
+    }
+  }
+};
+</script>
 
-<style lang="stylus" scoped>
-.todoBox {
-  background: #ccc;
-}
+
+<style lang="sass" scoped>
+.box
+  border: 1px solid #ccc
+  button
+    background: #fff
+    border: 1px solid #ccc
+    margin-left: 4px
+    line-height: 2
+     
 </style>
